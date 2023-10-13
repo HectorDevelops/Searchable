fbc1a03e3d892f7778d6552dbf06b565f076dde6
“HectorDevelops” <HectorDevelops@gmail.com>
2023-10-13 00:48:40 -0700

    Updated header title to resize upon screen resize.

--- Desktop/Projects/ImageSearch/src/components/Header.js
+++ Desktop/Projects/ImageSearch/src/components/Header.js
@@ -3,8 +3,8 @@ import React from "react";
 const Header = ({ children }) => {
   return (
     <div className="bg-gray-900 flex items-center py-10 dark:bg-neutral-900">
-      <div className=" mx-auto w-screen">
-        <h1 className="text-green-600 text-center text-8xl font-bold mb-5">
+      <div className=" mx-auto w-screen sm:w-screen md:w-3/4 lg:w-1/2 xl:w-1/3">
+        <h1 className="text-green-600 text-center sm:text-8xl md:text-8xl lg:text-8xl xl:text-8xl font-bold mb-5">
           Searchable
           {children}
         </h1>
